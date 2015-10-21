b25eb0b73ce66dd5a71df8bc4287c791fe8a8938
Martin Ryan <martin@DeviceM.local>
2015-10-21 13:00:05 -0500

    completed test code for playToFive()

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -40,16 +40,87 @@ function getWinner(playerMove,computerMove) {
     // Write code that will set winner to either 'player', 'computer', or 'tie' based on the values of playerMove and computerMove.
     // Assume that the only values playerMove and computerMove can have are 'rock', 'paper', and 'scissors'.
     // The rules of the game are that 'rock' beats 'scissors', 'scissors' beats 'paper', and 'paper' beats 'rock'.
-    /* YOUR CODE HERE */
+
+    if (playerMove === "rock") {
+        switch (computerMove) {
+            case "rock":
+                winner = "tie";
+            break;
+            case "paper":
+                winner = "computer"
+            break;
+            case "scissors":
+                winner = "player";
+            break;
+        }
+    } else if (playerMove === "paper") {
+        switch (computerMove) {
+            case "rock":
+                winner = "player";
+            break;
+            case "paper":
+                winner = "tie"
+            break;
+            case "scissors":
+                winner = "computer";
+            break;
+        }
+    } else if (playerMove === "scissors") {
+        switch (computerMove) {
+            case "rock":
+                winner = "computer";
+            break;
+            case "paper":
+                winner = "player"
+            break;
+            case "scissors":
+                winner = "tie";
+            break;
+        }
+    } 
     return winner;
 }
 
 function playToFive() {
-    console.log("Let's play Rock, Paper, Scissors");
-    var playerWins = 0;
-    var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
+    // This function should continue to play Rock Paper Scissors until either the
+    // player or the computer has won five times.
+    // After each 'round', display some text in the console indicating who played
+    // what, who won, and what the current scoreboard looks like.
+    // For example,
+    //  console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+    //  console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+  
+  console.log('Let\'s play Rock Paper Scissors');
+  var playerWins = 0;
+  var computerWins = 0;
+  
+    while (playerWins > 4 || computerWins > 4) {
+        var playerMove = getPlayerMove();
+        var computerMove = getComputerMove();
+        /* ask about the scope of variable "winner" in the following line. Is "winner" retained from the function call? */
+        var winner = getWinner(playerMove, computerMove);
+        
+        if (winner === "player") {
+            playerWins += 1;
+        } else if (winner === "computer") {
+            computerWins += 1;
+        }
+
+        /* ask if the following will "break" out of the switch or break out of the "playToFive" function entirely
+        switch (winner) {
+            case "player":
+                playerWins += 1
+            break;
+            case "computer":
+                computerWins += 1
+            break;
+        } */
+        
+        console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+        console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+    }
+
     return [playerWins, computerWins];
+
 }
 
